7aa8babcc67aecdf00ea1987c765ff2281ce119a
h1m <himanshu.yadav643@gmail.com>
2021-05-29 20:09:14 +0530

    added the ingress service and checked the connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+// api for getting the current user
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log("Listening on port 3000!!");
 });
