9ec501d31b6217a8d651f8375e8a9786036f332f
h1m <himanshu.yadav643@gmail.com>
2021-05-31 21:11:01 +0530

    added all the routes to the main index.ts file

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-// api for getting the current user
+// api for signing up the app
 router.post("/api/users/signup", (res, req) => {
   res.send("Hi there!");
 });
