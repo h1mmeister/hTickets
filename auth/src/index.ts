3e535316945905e020b4123545f71e921e737549
h1m <himanshu.yadav643@gmail.com>
2021-06-09 06:53:06 +0530

    connection between auth service and mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose, { mongo } from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -23,8 +24,25 @@ app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
+// Using the error handling middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!");
-});
+// connecting to mongodb using clusterIP as it is present in another pod
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    // console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!");
+  });
+};
+
+start();
