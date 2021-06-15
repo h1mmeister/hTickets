25a005e7a03561c593024a50cc88d365c9a060a2
h1m <himanshu.yadav643@gmail.com>
2021-06-15 23:56:59 +0530

    working on compare function to check hashed and stored password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -14,5 +14,11 @@ export class Passport {
     return `${buf.toString("hex")}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    // return true is the stored password is equal to the hashed password during sign in
+    return buf.toString("hex") === hashedPassword;
+  }
 }
