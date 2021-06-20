487a6d4f5ea185e28682946f26520881bea3a132
h1m <himanshu.yadav643@gmail.com>
2021-06-21 02:49:51 +0530

    added cookie sesison

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { json } from "body-parser";
 import mongoose, { mongo } from "mongoose";
+import cookieSession from "cookie-session";
 
 import "express-async-errors";
 import { currentUserRouter } from "./routes/current-user";
@@ -12,7 +13,17 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
+
 app.use(json());
+
+// adding cookie-session
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
