74e3cbcae97ad1582725aea8cc1fccf74a1c1e33
h1m <himanshu.yadav643@gmail.com>
2021-06-21 03:03:16 +0530

    added jwt to the session object

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -38,6 +39,21 @@ router.post(
     }
     const user = User.build({ email, password });
     await user.save();
+
+    // generate json web token
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+
+    // store the jwt on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
   }
 );
