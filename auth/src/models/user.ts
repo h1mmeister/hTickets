594ed0576c045f80b3bff7d25191812129e89041
h1m <himanshu.yadav643@gmail.com>
2021-06-22 23:50:23 +0530

    working on the structure of the properties of the user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -19,16 +19,29 @@ interface UserDoc extends mongoose.Document {
 }
 
 // we are creating the schema for the model class
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    // setting the properties sent back to the requester
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
