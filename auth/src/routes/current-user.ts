d7613583c3760af2cac116999da1a9ea5fc659cb
h1m <himanshu.yadav643@gmail.com>
2021-06-23 00:47:23 +0530

    working on the current user handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 // api for getting the current user
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  if (!req.session?.jwt) {
+    // !req.session || !req.session.jwt
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
