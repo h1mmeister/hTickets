cb6319e3cc488aa66c7996bdecb657ade699d2f8
h1mmeister <himanshu.yadav643@gmail.com>
2022-02-09 13:50:06 +0530

    handling errors with req body via express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,30 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 // creating a router
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there!');
-});
+// we are using express-validator to validate the incoming email and password request
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters!'),
+  ],
+  (req: Request, res: Response) => {
+    // checking if we have any errors within the request object
+    const errors = validationResult(req);
+
+    // handling those errors by sending back the response
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    console.log('Creating a user...');
+    res.send({});
+  }
+);
 
 export { router as signUpRouter };
