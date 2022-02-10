5de6cb5a206a79f0429454b325be18d82714cd28
h1mmeister <himanshu.yadav643@gmail.com>
2022-02-10 13:00:34 +0530

    added conditional checks to handle diff types of errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,12 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong!');
+  if (err instanceof RequestValidationError) {
+    console.log('handling request validation error!');
+  } else if (err instanceof DatabaseConnectionError) {
+    console.log('handling database connection error!');
+  }
+
   res.status(400).send({
     message: err.message,
   });
