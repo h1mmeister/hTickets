805c75a2550a7d4bb26624ccebf33f13abd23896
h1mmeister <himanshu.yadav643@gmail.com>
2022-02-14 17:11:16 +0530

    completed the response normalization techniques

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -17,7 +17,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters!'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     // checking if we have any errors within the request object
     const errors = validationResult(req);
 
