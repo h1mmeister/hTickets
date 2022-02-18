2e1718623aa05f00319d6e0934692fc3abe1f94e
h1mmeister <himanshu.yadav643@gmail.com>
2022-02-18 17:53:26 +0530

    connecting to mongodb

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
